7f495603d114202a9ef599169009f4fd971501bf
huangshubing <1484005289@qq.com>
2019-10-18 11:25:35 +0800

    修改了about页面

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="about">
     <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
+    <h1>about页面</h1>
     <NavBar msg="msg" />
   </div>
 </template>
